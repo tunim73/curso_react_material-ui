c5a0c8db745fae6e0db025074ac9530c11c18d66
tunim73 <antonio.netto973@gmail.com>
2023-05-17 16:36:06 -0300

    initial react router v6.11.2

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider } from "react-router-dom";
+import { AppRoutes } from './routes/index.tsx';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={AppRoutes}/>
+  </React.StrictMode>
 )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,16 @@
-export {}
+import {Navigate, createBrowserRouter} from "react-router-dom";
+import App from "../App";
+
+
+export const AppRoutes = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "*",
+      element: <Navigate to = '/'/>,
+    },
+  ]
+  );
